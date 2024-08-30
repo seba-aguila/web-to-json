bd979666849c38c8330c35b934fac34dc31f593a
victoramelinckx <vickx2000@gmail.com>
2024-08-30 10:54:42 -0400

    text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,10 @@ export default function Home() {
   return (
     <main className="flex h-screen flex-col items-center justify-center gap-2">
       <h1 className="text-center text-3xl font-bold leading-tight tracking-tighter md:text-6xl lg:leading-[1.1]">
-        Web to JSON
+        Web to Text/JSON
       </h1>
       <span className="max-w-[750px] text-center text-lg text-muted-foreground sm:text-xl">
-        Transform the text of any website to JSON with just one click!
+        Transform the text of any website to Text/JSON with just one click!
       </span>
       <Form />
     </main>
